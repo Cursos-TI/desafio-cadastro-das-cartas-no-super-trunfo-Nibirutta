19deb413cdd55e5de3ec98906439539e9f826d88
Lucas <lucas.s.augusto@hotmail.com>
2025-09-04 16:24:38 -0300

    Desafio Mestre Feito!

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,12 +5,14 @@ int main() {
     // Variaveis
 
     char state_1, code_1[5], city_1[50];
-    int populationAmount_1, sights_1;
-    float area_1, pib_1, pibCapita_1, populationDensity_1;
+    int sights_1;
+    float area_1, pib_1, pibCapita_1, populationDensity_1, superPower_1;
+    unsigned int populationAmount_1;
 
     char state_2, code_2[5], city_2[50];
-    int populationAmount_2, sights_2;
-    float area_2, pib_2, pibCapita_2, populationDensity_2;
+    int  sights_2;
+    float area_2, pib_2, pibCapita_2, populationDensity_2, superPower_2;
+    unsigned int populationAmount_2;
 
     // Leitura dos dados - Cidade 01
 
@@ -26,7 +28,7 @@ int main() {
     city_1[strcspn(city_1, "\n")] = 0; // Apaga a quebra de linha gerada pelo fgets, substituindo pelo 0.
 
     printf("Quantas pessoas vivem nessa cidade: \n");
-    scanf("%i", &populationAmount_1);
+    scanf("%u", &populationAmount_1);
 
     printf("Qual a area aproximada dessa cidade em quilometros quadrados: \n");
     scanf("%f", &area_1);
@@ -41,6 +43,8 @@ int main() {
     populationDensity_1 = (float) populationAmount_1 / area_1; // Densidade Populacional
 
     pibCapita_1 = (float) pib_1 / populationAmount_1; // PIB per Capita
+
+    superPower_1 = (float) populationAmount_1 + area_1 + pib_1 + sights_1 + pibCapita_1 + (1 / populationDensity_1); // Calculando o inverso da densidade populacional e somando ao restante, inclusive fazendo do cast
 
     getchar(); // Retira a quebra de linha do comando de leitura para que o próximo comando funcione corretamente, faça isso sempre que alternar entre leitura de chars, strings e numbers - float, int, double...
 
@@ -58,7 +62,7 @@ int main() {
     city_2[strcspn(city_2, "\n")] = 0; // Apaga a quebra de linha gerada pelo fgets, substituindo pelo 0.
 
     printf("Quantas pessoas vivem nessa cidade: \n");
-    scanf("%i", &populationAmount_2);
+    scanf("%u", &populationAmount_2);
 
     printf("Qual a area aproximada dessa cidade em quilometros quadrados: \n");
     scanf("%f", &area_2);
@@ -74,6 +78,8 @@ int main() {
 
     pibCapita_2 = (float) pib_2 / populationAmount_2; // PIB per Capita
 
+    superPower_2 = (float) populationAmount_2 + area_2 + pib_2 + sights_2 + pibCapita_2 + (1 / populationDensity_2); // Calculando o inverso da densidade populacional e somando ao restante, inclusive fazendo do cast
+
     // Apresentação dos dados - Cidade 01
 
     printf("\n");
@@ -81,7 +87,7 @@ int main() {
     printf("Estado: %c\n", state_1);
     printf("Código: %s\n", code_1);
     printf("Nome da cidade: %s\n", city_1);
-    printf("População: %i\n", populationAmount_1);
+    printf("População: %u\n", populationAmount_1);
     printf("Área (KM^2): %.2f\n", area_1);
     printf("PIB: %.2f R$\n", pib_1);
     printf("Número de pontos turísticos: %d\n", sights_1);
@@ -96,13 +102,26 @@ int main() {
     printf("Estado: %c\n", state_2);
     printf("Código: %s\n", code_2);
     printf("Nome da cidade: %s\n", city_2);
-    printf("População: %i\n", populationAmount_2);
+    printf("População: %u\n", populationAmount_2);
     printf("Área (KM^2): %.2f\n", area_2);
     printf("PIB: %.2f R$\n", pib_2);
     printf("Número de pontos turísticos: %d\n", sights_2);
     printf("Densidade Populacional: %.2f\n", populationDensity_2);
     printf("PIB per capita: %.2f R$\n", pibCapita_2);
     printf("\n");
+
+    // Comparação das cartas(cidades)
+
+    printf("Comparação das cartas.\n");
+    printf("População: Carta %d venceu! (%d)\n", 1 + (populationAmount_1 < populationAmount_2), populationAmount_1 < populationAmount_2);
+    printf("Área: Carta %d venceu! (%d)\n", 1 + (area_1 < area_2), area_1 < area_2);
+    printf("PIB: Carta %d venceu! (%d)\n", 1 + (pib_1 < pib_2), pib_1 < pib_2);
+    printf("Pontos turísticos: Carta %d venceu! (%d)\n", 1 + (sights_1 < sights_2), sights_1 < sights_2);
+    printf("Densidade Populacional: Carta %d venceu! (%d)\n", 1 + (populationDensity_1 > populationDensity_2), populationDensity_1 > populationDensity_2);
+    printf("PIB per capita: Carta %d venceu! (%d)\n", 1 + (pibCapita_1 < pibCapita_2), pibCapita_1 < pibCapita_2);
+    printf("Super Poder: Carta %d venceu! (%d)\n", 1 + (superPower_1 < superPower_2), superPower_1 < superPower_2);
+
+    // Como não foi dito nada a respeito do if no desafio, eu tentei fazer uma implementação sem essa estrutura, me aproveitando do valor retornado na comparação somando com 1 para definir a carta vencedora.
 
     return 0;
 }
